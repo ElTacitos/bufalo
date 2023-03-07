e732c65170665e366c2c653d50778e6c05d74eca
hugolocatelli <hugolo@padok.fr>
2023-03-07 22:52:56 +0100

    feat: basic fully random version

---
+++ app.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <span class="text">
+      Le cours actuel du bufalo est de:
+    </span>
+    <span class="value">
+      {{ currentValue }} gorgées
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const maxValue = 10;
+const minValue = 1;
+
+const currentValue = Math.floor(Math.random() * maxValue) + minValue;
+</script>
+
+<style>
+html {
+  background-color: #2C3333;
+  color: #CBE4DE;
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.text {
+  font-size: 1.5rem;
+}
+
+.value {
+  font-size: 2rem;
+  font-weight: bold;
+}
+</style>
