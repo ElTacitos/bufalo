f5696b69cc3df84e3aa64a373028a4b3da4e1669
hugolocatelli <hugolo@padok.fr>
2023-03-08 00:38:38 +0100

    feat: fully random but with a graph

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Line :data="data" :options="options" />
     <span class="text">
       Le cours actuel du bufalo est de:
     </span>
@@ -10,10 +11,71 @@
 </template>
 
 <script setup lang="ts">
-const maxValue = 10;
-const minValue = 1;
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
 
-const currentValue = Math.floor(Math.random() * maxValue) + minValue;
+const maxValue = 10;
+const minValue = 2;
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+function getLast5minLabels() {
+  const labels = [];
+  for (let i = 0; i < 5; i++) {
+    labels.push(new Date(Date.now() - 60*1000*i).toLocaleTimeString());
+  }
+  return labels.reverse();
+}
+
+function getRandomValue(count: number) {
+  const values = [];
+  for (let i = 0; i < count; i++) {
+    values.push(Math.floor(Math.random() * (maxValue-minValue)) + minValue);
+  }
+
+  return values;
+}
+const values = getRandomValue(5);
+const currentValue = values[values.length - 1];
+const data = {
+  labels: getLast5minLabels(),
+  datasets: [
+    {
+      label: 'Cours du bufalo',
+      backgroundColor: '#f87979',
+      data: values,
+      fill: false,
+      clip: {left: false, top: 50, right: false, bottom: false}
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  scales: {
+    y: {
+      min: 0,
+      max: 12,
+      ticks: {
+        stepSize: 1
+      }
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  }
+}
 </script>
 
 <style>
@@ -36,11 +98,12 @@ body {
 }
 
 .text {
-  font-size: 1.5rem;
+  margin-top: 5rem;
+  font-size: 1rem;
 }
 
 .value {
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: bold;
 }
 </style>
